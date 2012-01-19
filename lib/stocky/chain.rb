fc38d5a64e57b8c5c21c2eefc3db3ea1355f3bfd
kbl <m.pietraszek@gmail.com>
2012-01-19 12:21:06 +0100

    specs for counting probability

--- lib/stocky/chain.rb
+++ lib/stocky/chain.rb
@@ -1,11 +1,17 @@
 module Stocky
   class Chain
 
+    UP = :up
+    DOWN = :down
+
+    # number of transitions is lower than number of elements
+    TRANSITIONS = 1
+
     attr_reader :trend, :probability
 
     def initialize(input)
       @trend = input.each_cons(2).map do |early_price, late_price|
-        early_price < late_price
+        early_price < late_price ? UP : DOWN
       end
 
       @probability = @trend
@@ -14,10 +20,18 @@ module Stocky
           hash[trend_pair] += 1
           hash
         end
-        .inject({}) do |hash, (key, count)|
-          hash[key] = count / @trend.length
+        .inject(Hash.new(0)) do |hash, (key, count)|
+          hash[key] = count / (@trend.length.to_f - TRANSITIONS)
           hash
         end
+    end
+
+    def predict(last_trend)
+      [UP, DOWN].inject({}) do |hash, trend|
+        key = [last_trend, trend]
+        hash[trend] = @probability[key]
+        hash
+      end
     end
 
   end
--- spec/stocky/chain_spec.rb
+++ spec/stocky/chain_spec.rb
@@ -1,15 +1,24 @@
+require 'spec_helper'
+
 module Stocky
   describe Chain do
 
     it 'should properly count trend' do
       chain = Chain.new([1, 2, 3, 4, 3, 2, 2, 4, 1, 3])
-      chain.trend.must_equal [true, true, true, false, false, false, true, false, true]
+      chain.trend.must_equal [:up, :up, :up, :down, :down, :down, :up, :down, :up]
     end
 
-    it 'should properly count probability' do
-      chain = Chain.new([1, 2, 3, 4, 3, 2, 2, 4, 1, 3, 2])
-      chain.trend.must_equal [true, true, true, false, false, false, true, false, true, false]
-      chain.probability.must_equal []
+    describe 'probability' do
+      it 'is certain that will go up' do
+        chain = Chain.new([1, 2, 3])
+        chain.predict(:up).must_equal({up: 1, down: 0})
+        chain.predict(:down).must_equal({up: 0, down: 0})
+      end
+      it 'is certain that will go down' do
+        chain = Chain.new([3, 2, 1])
+        chain.predict(:up).must_equal({up: 0, down: 0})
+        chain.predict(:down).must_equal({up: 0, down: 1})
+      end
     end
 
   end
